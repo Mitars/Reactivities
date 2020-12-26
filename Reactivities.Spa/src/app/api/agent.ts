c2945c1e83d5acb674127272cf499b0671ab3776
Mitar Stjepcevic <Mitarkun.com>
2020-12-26 18:31:34 +0100

    Remove unused function

--- Reactivities.Spa/src/app/api/agent.ts
+++ Reactivities.Spa/src/app/api/agent.ts
@@ -58,11 +58,6 @@ axios.interceptors.response.use(
 
 const responseBody = (response: AxiosResponse) => response.data;
 
-const sleep = (ms: number) => (response: AxiosResponse) =>
-  new Promise<AxiosResponse>((resolve) =>
-    setTimeout(() => resolve(response), ms)
-  );
-
 const requests = {
   get: (url: string) => axios.get(url).then(responseBody),
   post: (url: string, body: {}) => axios.post(url, body).then(responseBody),
